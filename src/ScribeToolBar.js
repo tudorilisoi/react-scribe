f477d49bb38657cc3d7c157b452301a8055ad58d
Tudor Ilisoi <tudorilisoi@yahoo.com>
2016-06-01 17:39:05 +0300

    text in toolbar

--- src/ScribeToolBar.js
+++ src/ScribeToolBar.js
@@ -5,47 +5,57 @@ import React, { PropTypes } from 'react';
  */
 
 class ScribeToolbar extends React.Component {
-  render () {
-    let toolBarOptions = [];
-    for (const i in this.props.config) {
-      toolBarOptions.push(
-        <button data-command-name={ this.props.config[i]['command'] } key={ i } >
-          <i className={ `fa ${this.props.config[i]['display']}` } />
-        </button>
-      );
-    }
+	render() {
+		let toolBarOptions = [];
+		for (const i in this.props.config) {
+			let iconOrText
+			if (this.props.config[i].text) {
+				iconOrText = (
+					<i className="fa">
+						{this.props.config[i].text}
+					</i>)
+			} else {
+				iconOrText = (<i className={ `fa ${this.props.config[i]['display']}` }/>)
 
-    return (
-      <div className='sc-toolbar'>
-        <button data-command-name='bold'>
-          <i className='fa fa-bold' />
-        </button>
-        <button data-command-name='italic'>
-          <i className='fa fa-italic' />
-        </button>
-        <button data-command-name='underline'>
-          <i className='fa fa-underline' />
-        </button>
-        { toolBarOptions }
-        <button data-command-name='undo'>
-          <i className='fa fa-undo' />
-        </button>
-        <button data-command-name='redo'>
-          <i className='fa fa-repeat' />
-        </button>
-      </div>
-    );
-  }
+			}
+			toolBarOptions.push(
+				<button data-command-name={ this.props.config[i]['command'] } key={ i }>
+					{iconOrText}
+				</button>
+			);
+		}
+
+		return (
+			<div className='sc-toolbar'>
+				<button data-command-name='bold'>
+					<i className='fa fa-bold'/>
+				</button>
+				<button data-command-name='italic'>
+					<i className='fa fa-italic'/>
+				</button>
+				<button data-command-name='underline'>
+					<i className='fa fa-underline'/>
+				</button>
+				{ toolBarOptions }
+				<button data-command-name='undo'>
+					<i className='fa fa-undo'/>
+				</button>
+				<button data-command-name='redo'>
+					<i className='fa fa-repeat'/>
+				</button>
+			</div>
+		);
+	}
 }
 
 ScribeToolbar.propTypes = {
-  config: PropTypes.shape({
-    commands: PropTypes.arrayOf(PropTypes.shape({
-      commmand: PropTypes.string.isRequired,
-      display: PropTypes.string.isRequired,
-      action: PropTypes.func
-    }))
-  })
+	config: PropTypes.shape({
+		commands: PropTypes.arrayOf(PropTypes.shape({
+			commmand: PropTypes.string.isRequired,
+			display: PropTypes.string.isRequired,
+			action: PropTypes.func
+		}))
+	})
 };
 
 export default ScribeToolbar;
--- src/optionMap.js
+++ src/optionMap.js
@@ -6,82 +6,88 @@ import UnlinkCmd from 'scribe-plugin-intelligent-unlink-command';
 
 /**
  * Hashmap of commands
+ * NOTE text property takes over display property
  */
 
 const optionMap = {
-  'blockquote': {
-    'command': 'blockquote',
-    'action': BlockQuoteCmd(),
-    'display': 'fa-quote-left'
-  },
-  'code': {
-    'command': 'code',
-    'action': CodeCmd(),
-    'display': 'fa-code'
-  },
-  'h1': {
-    'command': 'h1',
-    'action': HeadingCmd(1),
-    'display': 'fa-header'
-  },
-  'h2': {
-    'command': 'h2',
-    'action': HeadingCmd(2),
-    'display': 'fa-header'
-  },
-  'h3': {
-    'command': 'h3',
-    'action': HeadingCmd(3),
-    'display': 'fa-header'
-  },
-  'h4': {
-    'command': 'h4',
-    'action': HeadingCmd(4),
-    'display': 'fa-header'
-  },
-  'h5': {
-    'command': 'h5',
-    'action': HeadingCmd(5),
-    'display': 'fa-header'
-  },
-  'linkPrompt': {
-    'command': 'linkPrompt',
-    'action': LinkPromptCmd(),
-    'display': 'fa-link'
-  },
-  'unlink': {
-    'command': 'unlink',
-    'action': UnlinkCmd(),
-    'display': 'fa-unlink'
-  },
-  'ol': {
-    'command': 'insertOrderedList',
-    'display': 'fa-list-ol'
-  },
-  'ul': {
-    'command': 'insertUnOrderedList',
-    'display': 'fa-list-ul'
-  },
-  'indent': {
-    'command': 'indent',
-    'display': 'fa-indent'
-  },
-  'outdent': {
-    'command': 'outdent',
-    'display': 'fa-outdent'
-  },
-  'superscript': {
-    'command': 'superscript',
-    'display': 'fa-superscript'
-  },
-  'subscript': {
-    'command': 'subscript',
-    'display': 'fa-subscript'
-  },
-  'cleanup': {
-    'command': 'cleanup',
-    'display': 'fa-eraser'
-  }
+	'blockquote': {
+		'command': 'blockquote',
+		'action': BlockQuoteCmd(),
+		'display': 'fa-quote-left'
+	},
+	'code': {
+		'command': 'code',
+		'action': CodeCmd(),
+		'display': 'fa-code'
+	},
+	'h1': {
+		'command': 'h1',
+		'action': HeadingCmd(1),
+		'display': 'fa-header',
+		'text': 'H1',
+	},
+	'h2': {
+		'command': 'h2',
+		'action': HeadingCmd(2),
+		'display': 'fa-header',
+		'text': 'H2',
+	},
+	'h3': {
+		'command': 'h3',
+		'action': HeadingCmd(3),
+		'display': 'fa-header',
+		'text': 'H3',
+	},
+	'h4': {
+		'command': 'h4',
+		'action': HeadingCmd(4),
+		'display': 'fa-header',
+		'text': 'H4',
+	},
+	'h5': {
+		'command': 'h5',
+		'action': HeadingCmd(5),
+		'display': 'fa-header',
+		'text': 'H5',
+	},
+	'linkPrompt': {
+		'command': 'linkPrompt',
+		'action': LinkPromptCmd(),
+		'display': 'fa-link'
+	},
+	'unlink': {
+		'command': 'unlink',
+		'action': UnlinkCmd(),
+		'display': 'fa-unlink'
+	},
+	'ol': {
+		'command': 'insertOrderedList',
+		'display': 'fa-list-ol'
+	},
+	'ul': {
+		'command': 'insertUnOrderedList',
+		'display': 'fa-list-ul'
+	},
+	'indent': {
+		'command': 'indent',
+		'display': 'fa-indent'
+	},
+	'outdent': {
+		'command': 'outdent',
+		'display': 'fa-outdent'
+	},
+	'superscript': {
+		'command': 'superscript',
+		'display': 'fa-superscript'
+	},
+	'subscript': {
+		'command': 'subscript',
+		'display': 'fa-subscript'
+	},
+	'cleanup': {
+		'command': 'cleanup',
+		'display': 'fa-eraser'
+	}
 };
 
 export default optionMap;
